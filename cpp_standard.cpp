b88f509f5545625d6f2c5f5c595221c540fa2fab
duck-wsl <duke@duke>
2023-09-01 12:07:00 +0700

    checking WSL

--- cpp_standard.cpp
+++ cpp_standard.cpp
@@ -5,5 +5,6 @@ int main(int argc, char const *argv[])
 {
     std::cout << "Hello World" << std::endl;
     std::cout << "C++ Standard Version: " << __cplusplus << std::endl;
+    /*comment*/
     return 0;
 }
